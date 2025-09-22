c1bdf0e8d0ab6cf9a1b9647c0cce312155533c55
Joonas Kajava <5013522+JoonasKajava@users.noreply.github.com>
2025-09-22 09:48:35 +0300

    fix: fix reactor

--- reactor/startup.lua
+++ reactor/startup.lua
@@ -2,16 +2,27 @@ local max_energy = 9000000
 
 local reactor = peripheral.wrap("left")
 
+local function clamp(num, min, max)
+    if num < min then
+        return min
+    elseif num > max then
+        return max
+    else
+        return num
+    end
+end
+
 local function control()
     local energy = reactor.getEnergyStored()
     local per = energy / max_energy
-    return per * 100
+    return clamp(per * 100, 0, 100)
 end
+
 
 while(reactor.getActive())
 do
     local c = control()
-    print(c)
+    print(string.format("Power output at: %d", 100 - c))
     reactor.setAllControlRodLevels(c)
     sleep(1)
 end
